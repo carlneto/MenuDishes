52116af5d0c15b29043eb08811a920c66d3cc4c4
carlneto <carlneto@icloud.com>
2024-09-20 21:11:57 +0100

    Initial Commit

---
+++ MenuDishes/MenuDishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuDishesApp.swift
+//  MenuDishes
+//
+//  Created by Carlos Neto on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuDishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
